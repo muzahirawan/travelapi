279d93c73899f6978f80c397a3a5e0fd2b4056c1
muzahiawan <muzahirawan@gmail.com>
2023-04-17 19:08:12 +0500

    integrated dry principal

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -2,108 +2,73 @@ require 'dotenv'
 require 'json'
 
 class RoutesController < ApplicationController
-end
+  before_action :load_location, only: [:create, :update]
 
-Dotenv.load
+  def index
+    @routes = Route.select([:id, :routeSummary, :totalTravelTime, :travelMode, :totalTravelDistance])
+                  .where(origin: params['origin'], destination: params['destination'])
+    render json: @routes
+  rescue => err
+    logger.error err
+    render json: "Something went wrong"
+  end
 
-class RoutesController < ApplicationController
-    def index
-        begin 
-            @routes = Route.select([:id, :routeSummary, :totalTravelTime, :travelMode, :totalTravelDistance]).where(origin: params['origin'], destination: params['destination'])
-            render json: @routes
-        rescue Exception
+  def show
+    @route = Route.find(params[:id])
+    render json: @route
+  rescue => err
+    logger.error err
+    render json: "Something went wrong"
+  end
 
-            render json: "Something went wrong"
-        end
-    end
-
-    def show
-        begin
-            @routes = Route.find(params[:id])
-            render json: @routes
-        rescue => err
-            logger.error err
-            render json: "Something went wrong"
-
-        end
-    end
+  def create
+    @response = RestClient.get "#{ENV["GOOGLE_DIRECTIONS_API"]}?origin=#{params["start"]}&destination=#{params["end"]}&key=#{ENV["GOOGLE_API_KEY"]}"
+    @parsedResponse = JSON.parse(@response.body)
     
-    def create
-        begin
-            @location = Location.find(params["locationId"])
-            logger.info @location
-            @response =  RestClient.get "#{ENV["GOOGLE_DIRECTIONS_API"]}?origin=#{params["start"]}&destination=#{params["end"]}&key=#{ENV["GOOGLE_API_KEY"]}"
-            @parsedResponse = JSON.parse(@response.body)
-            
-            if @parsedResponse["status"] == "REQUEST_DENIED"
-                logger.error @parsedResponse
-                return render :json => {:errors =>  "Something went wrong"}, :code => 403
-            end
-            for route in @parsedResponse["routes"] do
-                logger.info route
-                temp = Route.create(
-                        origin: params["start"],
-                        destination: params["end"],
-                        routeSummary: route["summary"],
-                        data: route.to_json,
-                        location: @location,
-                        totalTravelTime: route["legs"][0]["distance"]["text"],
-                        travelMode: route["legs"][0]["duration"]["text"],
-                        totalTravelDistance: route["legs"][0]["steps"][0]["travel_mode"],
-                    )
-                temp.save
-            end
-            render json: "Success"
-        rescue => e
-            logger.error e.message
-            logger.error e.backtrace.join("\n")
-            render json: "Something went wrong"
-        end
-
+    if @parsedResponse["status"] == "REQUEST_DENIED"
+      logger.error @parsedResponse
+      return render json: { errors: "Something went wrong" }, code: 403
     end
 
-    def update
-        begin
-            Route.select([:id]).where(origin: params['start'], destination: params['end']).destroy_all()
-            @location = Location.find(params["locationId"])
-            logger.info @location
-            @response =  RestClient.get "#{ENV["GOOGLE_DIRECTIONS_API"]}?origin=#{params["start"]}&destination=#{params["end"]}&key=#{ENV["GOOGLE_API_KEY"]}"
-            @parsedResponse = JSON.parse(@response.body)
-            
-            if @parsedResponse["status"] == "REQUEST_DENIED"
-                logger.error @parsedResponse
-                return render :json => {:errors =>  "Something went wrong"}, :code => 403
-            end
-            for route in @parsedResponse["routes"] do
-                logger.info route
-                temp = Route.create(
-                        origin: params["start"],
-                        destination: params["end"],
-                        routeSummary: route["summary"],
-                        data: route.to_json,
-                        location: @location,
-                        totalTravelTime: route["legs"][0]["distance"]["text"],
-                        travelMode: route["legs"][0]["duration"]["text"],
-                        totalTravelDistance: route["legs"][0]["steps"][0]["travel_mode"],
-                    )
-                temp.save
-            end
-            render json: "Success"
-        rescue => e
-            logger.error e.message
-            logger.error e.backtrace.join("\n")
-            render json: "Something went wrong"
-        end    
+    @parsedResponse["routes"].each do |route|
+      temp = Route.create(
+        origin: params["start"],
+        destination: params["end"],
+        routeSummary: route["summary"],
+        data: route.to_json,
+        location: @location,
+        totalTravelTime: route["legs"][0]["distance"]["text"],
+        travelMode: route["legs"][0]["duration"]["text"],
+        totalTravelDistance: route["legs"][0]["steps"][0]["travel_mode"]
+      )
+      temp.save
     end
 
-    def destroy
-        begin
-            @location =Route.find(params[:id])
-            @location.destroy
-            render json: "#{@location.id} has been deleted! " 
-        rescue Exception
-            render json: "Something went wrong"
-        end
+    render json: "Success"
+  rescue => e
+    logger.error e.message
+    logger.error e.backtrace.join("\n")
+    render json: "Something went wrong"
+  end
 
-    end
+  def update
+    Route.where(origin: params['start'], destination: params['end']).destroy_all()
+    create
+  end
+
+  def destroy
+    @route = Route.find(params[:id])
+    @route.destroy
+    render json: "#{params[:id]} has been deleted!"
+  rescue => err
+    logger.error err
+    render json: "Something went wrong"
+  end
+
+  private
+
+  def load_location
+    @location = Location.find(params["locationId"])
+    logger.info @location
+  end
 end
